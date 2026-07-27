536e626a19747e37f3db545d85aeb252ce29d63f
krrishmahar <krrishmahar5@gmail.com>
2026-07-27 07:43:33 +0530

    validate BST && Flatten BST to Linkedlist

--- src/practice/tree/medium/FlattenBSTLinkedlist.java
+++ src/practice/tree/medium/FlattenBSTLinkedlist.java
@@ -1,4 +1,24 @@
 package practice.tree.medium;
 
+import practice.tree.TreeNode;
+
 public class FlattenBSTLinkedlist {
+    public void flatten(TreeNode root) {
+        if (root == null)   return;
+        TreeNode curr = root;
+        while (curr != null){
+            //find rightmost element from left -> right subtree
+            if (curr.left != null){
+                TreeNode temp = curr.left;
+                while (temp.right != null){
+                    temp = temp.right;
+                }
+
+                temp.right = curr.right;
+                curr.right = curr.left;
+                curr.left  = null;
+            }
+            curr = curr.right;
+        }
+    }
 }
---
+++ src/practice/tree/medium/LowestCommonAncestor.java
@@ -0,0 +1,10 @@
+package practice.tree.medium;
+
+import practice.tree.TreeNode;
+
+//https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+public class LowestCommonAncestor {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+    }
+}
