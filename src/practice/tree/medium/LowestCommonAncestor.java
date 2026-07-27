81878099082e0eb093dcec4b192b3dbcc8dd1e87
krrishmahar <krrishmahar5@gmail.com>
2026-07-27 08:08:03 +0530

    added new function

--- src/practice/tree/medium/LowestCommonAncestor.java
+++ src/practice/tree/medium/LowestCommonAncestor.java
@@ -3,8 +3,18 @@ package practice.tree.medium;
 import practice.tree.TreeNode;
 
 //https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+//Time Complexity: O(n)
+//Space Complexity: O(H) or O(log n)
 public class LowestCommonAncestor {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        //LowestCommonAncestor can be a node itself
+        if (root == null)   return null;
+        if (root == p || root == q) return root;
 
+        TreeNode left = lowestCommonAncestor(root.left, p,q);
+        TreeNode right = lowestCommonAncestor(root.right, p,q);
+
+        if (left != null && right != null)  return root;
+        return left == null ? right : left;
     }
 }
