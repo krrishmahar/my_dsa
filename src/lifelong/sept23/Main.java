f86c7cfabf937b263ae51928e5447819a3afdbf4
krrishmahar <krrishmahar5@gmail.com>
2026-09-23 07:17:29 +0530

    lifelong series

---
+++ src/lifelong/sept23/Main.java
@@ -0,0 +1,16 @@
+package lifelong.sept23;
+
+public class Main {
+    /*
+    Questions I did today were:
+        - Find the Duplicate Number
+        - Duplicate Zeros
+        - Remove Duplicates from Sorted Array II
+        - Remove Duplicates from Sorted List II
+        - Remove Duplicates from Sorted List
+    */
+
+    static void main() {
+
+    }
+}
